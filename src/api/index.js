6711ee4f2f28c3f5f66e17770e5a61ca5f96957c
Shahed Nasser <shahednasser@gmail.com>
2021-12-13 10:51:04 +0200

    added new admin endpoint for part 3

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,26 @@
+import bodyParser from "body-parser"
+import cors from "cors"
 import { Router } from "express"
+import { projectConfig } from "../../medusa-config"
 
 export default () => {
   const router = Router()
 
+  const corsOptions = {
+    origin: projectConfig. admin_cors.split(","),
+    credentials: true,
+  }
+
   router.get("/store/top-products", async (req, res) => {
+    const topProductsService = req.scope.resolve("topProductsService")
+
+    res.json({
+      products: await topProductsService.getTopProducts()
+    })
+  })
+
+  router.options('/admin/top-products', cors(corsOptions))
+  router.get("/admin/top-products", cors(corsOptions), bodyParser.json(), async (req, res) => {
     const topProductsService = req.scope.resolve("topProductsService")
 
     res.json({
